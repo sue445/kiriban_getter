cbc6f12b481f49348c9e58b6e6ff1ced0b3e5c46
sue445 <sue445@sue445.net>
2016-06-09 23:12:47 +0900

    Impl zorome?

--- lib/kiriban.rb
+++ lib/kiriban.rb
@@ -5,5 +5,10 @@ module Kiriban
     def kiriban?
       !!(self.to_s =~ /^[1-9]0+$/)
     end
+
+    def zorome?
+      return false if self < 10
+      self.to_s.each_char.map(&:itself).uniq.count == 1
+    end
   end
 end
--- spec/kiriban_spec.rb
+++ spec/kiriban_spec.rb
@@ -19,4 +19,25 @@ describe Kiriban do
       it { should eq expected }
     end
   end
+
+  describe "#zorome?" do
+    subject { number.zorome? }
+
+    using Kiriban
+    using RSpec::Parameterized::TableSyntax
+
+    where(:number, :expected) do
+      1    | false
+      10   | false
+      11   | true
+      200  | false
+      222  | true
+      1122 | false
+      3000 | false
+    end
+
+    with_them do
+      it { should eq expected }
+    end
+  end
 end
